8fcf4054867cef6ce5548472073b608fe3bcd087
ruben <rubeensanchoopuigg18@gmail.com>
2024-09-24 12:26:27 +0200

    Loginero toca JS

--- hellogit.js
+++ hellogit.js
@@ -1,5 +1,5 @@
 function funcionprueba() {
 
-    console.log("Hola desarrollo la rama main");
+    console.log("Hola desarrollo la rama main PERO SOY UN MANAZAS DE LA RAMA LOGIN TOCANDO COSAS K NO SE TIENEN K TOCAR JIJI");
 
 }
